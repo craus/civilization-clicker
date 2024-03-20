c48d85a76afcfb5dcbaf59b0b429459c5365d3bd
craus90 <craus902@gmail.com>
2024-03-20 13:23:32 +0400

    faster predictions; check prediction of commands

--- market.js
+++ market.js
@@ -53,7 +53,7 @@ function market(verb, id, generateDeal)
       return this.level >= 1
     },
     remainingTime: function() {
-      return timeUntil(() => resources[from]()+this.deal.change[from] >= 0)
+      return timeUntil(() => resources[from]()+this.deal.change[from] >= 0 && resources.commands() >= 1)
     },
     paint: function() {
       panel.find('.level').text(this.level)
--- timeUntil.js
+++ timeUntil.js
@@ -1,5 +1,17 @@
 timeUntil = function(predicate) {
   civilization.backup()
+  
+  for (let i = 0; i < 100; i++) {
+    civilization.tickTime(1e10)
+  }
+  
+  if (!predicate()) {
+    civilization.restore()
+    return Number.POSITIVE_INFINITY
+  }
+  
+  civilization.restore()
+  
   for (let i = 0; i < 1e6; i++) {
     civilization.tickTime(0.1)
     if (predicate()) {
